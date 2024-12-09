750e87b1d0e03ec49b1a006c511808756e5b92f5
Turis-Tv <turistv3@gmail.com>
2024-12-09 16:50:37 +0100

    Add files via upload

---
+++ Ana-Dosya/10000-XXX/3-Player/script.js
@@ -0,0 +1,364 @@
+const channels = [
+  {
+    channelName: "XXX 101",
+    src: "https://vod.mycamtv.net/101.m3u8 ",
+  },
+  {
+    channelName: "XXX 102",
+    src: "https://vod.mycamtv.net/102.m3u8 ",
+  },
+    {
+    channelName: "XXX 103",
+    src: "https://vod.mycamtv.net/103.m3u8",
+  },
+    {
+    channelName: "XXX 104",
+    src: "https://vod.mycamtv.net/104.m3u8 ",
+  },
+    {
+    channelName: "XXX 105",
+    src: "https://vod.mycamtv.net/105.m3u8 ",
+  },
+    {
+    channelName: "XXX 106",
+    src: "https://vod.mycamtv.net/106.m3u8 ",
+  },
+    {
+    channelName: "XXX 107",
+    src: "https://vod.mycamtv.net/107.m3u8 ",
+  },
+    {
+    channelName: "XXX 108",
+    src: " https://vod.mycamtv.net/108.m3u8 ",
+  },
+     {
+    channelName: "XXX 109",
+    src: " https://vod.mycamtv.net/109.m3u8 ",
+  },
+     {
+    channelName: "XXX 110",
+    src: " https://vod.mycamtv.net/110.m3u8 ",
+  },
+     {
+    channelName: "XXX 111",
+    src: " https://vod.mycamtv.net/111.m3u8 ",
+  },
+     {
+    channelName: "XXX 112",
+    src: " https://vod.mycamtv.net/112.m3u8  ",
+  },
+     {
+    channelName: "XXX 113",
+    src: " https://vod.mycamtv.net/113.m3u8 ",
+  },
+     {
+    channelName: "XXX 114",
+    src: "https://vod.mycamtv.net/114.m3u8  ",
+  },
+     {
+    channelName: "XXX 115",
+    src: "https://vod.mycamtv.net/115.m3u8  ",
+  },
+     {
+    channelName: "XXX 116",
+    src: "https://vod.mycamtv.net/116.m3u8  ",
+  },
+     {
+    channelName: "XXX 117",
+    src: "https://vod.mycamtv.net/117.m3u8  ",
+  },
+     {
+    channelName: "XXX 118",
+    src: "https://vod.mycamtv.net/118.m3u8  ",
+  },
+     {
+    channelName: "XXX 119",
+    src: "https://vod.mycamtv.net/119.m3u8  ",
+  },
+     {
+    channelName: "XXX 120",
+    src: "https://vod.mycamtv.net/120.m3u8  ",
+  },
+     {
+    channelName: "XXX 121",
+    src: " https://vod.mycamtv.net/121.m3u8 ",
+  },
+     {
+    channelName: "XXX 122",
+    src: "https://vod.mycamtv.net/122.m3u8  ",
+  },
+     {
+    channelName: "XXX 123",
+    src: " https://vod.mycamtv.net/123.m3u8 ",
+  },
+     {
+    channelName: "XXX 124",
+    src: "https://vod.mycamtv.net/124.m3u8  ",
+  },
+     {
+    channelName: "XXX 125",
+    src: "https://vod.mycamtv.net/125.m3u8  ",
+  },
+     {
+    channelName: "XXX 126",
+    src: "https://vod.mycamtv.net/126.m3u8  ",
+  },
+     {
+    channelName: "XXX 127",
+    src: "https://vod.mycamtv.net/127.m3u8  ",
+  },
+     {
+    channelName: "XXX 128",
+    src: " https://vod.mycamtv.net/128.m3u8 ",
+  },
+     {
+    channelName: "XXX 129",
+    src: "https://vod.mycamtv.net/129.m3u8  ",
+  },
+     {
+    channelName: "XXX 130",
+    src: "https://vod.mycamtv.net/130.m3u8  ",
+  },
+     {
+    channelName: "XXX 131",
+    src: "https://vod.mycamtv.net/131.m3u8  ",
+  },
+     {
+    channelName: "XXX 132",
+    src: "https://vod.mycamtv.net/132.m3u8  ",
+  },
+     {
+    channelName: "XXX 133",
+    src: "https://vod.mycamtv.net/133.m3u8  ",
+  },
+  {
+    channelName: "XXX 134",
+    src: "https://vod.mycamtv.net/134.m3u8 ",
+  },
+  {
+    channelName: "Turis",
+    src: "https://vod.mycamtv.net/135.m3u8 ",
+  },
+  {
+    channelName: "XXX 136",
+    src: "https://vod.mycamtv.net/136.m3u8 ",
+  },
+  {
+    channelName: "XXX 137",
+    src: "https://vod.mycamtv.net/137.m3u8 ",
+  },
+  {
+    channelName: "XXX 138",
+    src: " https://vod.mycamtv.net/138.m3u8",
+  },
+  {
+    channelName: "XXX 139",
+    src: "https://vod.mycamtv.net/139.m3u8 ",
+  },
+  {
+    channelName: "XXX 140",
+    src: "https://vod.mycamtv.net/140.m3u8 ",
+  },
+  {
+    channelName: "XXX 141",
+    src: "https://vod.mycamtv.net/141.m3u8  ",
+  },
+  {
+    channelName: "XXX 142",
+    src: "https://vod.mycamtv.net/142.m3u8 ",
+  },
+  {
+    channelName: "XXX 143",
+    src: "https://vod.mycamtv.net/143.m3u8 ",
+  },
+  {
+    channelName: "XXX 144 ",
+    src: "https://vod.mycamtv.net/144.m3u8 ",
+  },
+  {
+    channelName: "XXX 145",
+    src: "https://vod.mycamtv.net/145.m3u8 ",
+  },
+  {
+    channelName: "XXX 146",
+    src: "https://vod.mycamtv.net/146.m3u8 ",
+  },
+  {
+    channelName: "XXX 147",
+    src: "https://vod.mycamtv.net/147.m3u8 ",
+  },
+  {
+    channelName: "XXX 148",
+    src: "https://vod.mycamtv.net/148.m3u8 ",
+  },
+
+  {
+    channelName: "XXX 149",
+    src: "https://vod.mycamtv.net/149.m3u8 ",
+  },
+
+  {
+    channelName: "XXX 150",
+    src: "https://vod.mycamtv.net/150.m3u8",
+  },
+];
+
+const jwtConfigSetup = {
+  file: channels.find((c) => c.channelName === "Turis").src,
+  width: "100%",
+  height: "100%",
+  skin: "glow",
+  autostart: true,
+  primary: "flash",
+  fallback: "false",
+  abouttext: "",
+  aboutlink: "",
+  displaytitle: "false",
+  ga: "{}",
+};
+
+const getPlayerWrapper = () =>
+  document.querySelector("#content #player-wrapper #jwplayer-wrapper");
+const getBbcArabicIframe = () => document.querySelector("#bbc-iframe");
+
+const getElementByDataSrc = (dataSrc) =>
+  document.querySelector(`[data-src="${dataSrc}"]`);
+
+let currentlyPlayingSrc = null;
+const handleHighlightingSelectedChannel = (_src, channelsList) => {
+  const currentElement = getElementByDataSrc(_src);
+  if (currentElement) {
+    [...channelsList.children].forEach((el) => {
+      const _el =
+        el.querySelector(".channel-name") || el.querySelector(".bbc-arabic");
+      if (_el?.getAttribute("id")?.includes("selected-channel-indicator")) {
+        _el.setAttribute(
+          "id",
+          _el.getAttribute("id").replace("selected-channel-indicator", "")
+        );
+      }
+    });
+    currentElement.setAttribute("id", "selected-channel-indicator");
+  }
+  if (_src) {
+    currentlyPlayingSrc = _src;
+  }
+};
+
+const stopBBCPlayer = () => {
+  setTimeout(() => {
+    getBbcArabicIframe().style.display = "none";
+    const iframeSrc = getBbcArabicIframe().src;
+    getBbcArabicIframe().src = iframeSrc;
+  }, 0);
+};
+
+function main() {
+  loadContent();
+}
+
+function loadContent() {
+  const jwpInstance = jwplayer("jwplayer-wrapper");
+  jwpInstance.setup(jwtConfigSetup);
+
+  const getSrcElement = () => document.querySelector("#player-src");
+  const channelsList = document.querySelector("#channels-list");
+  const inputWrapper = document.querySelector("#input-wrapper");
+
+  const bbcArabicButton = document.querySelector(".bbc-arabic");
+  bbcArabicButton.addEventListener("click", () => {
+    setTimeout(() => {
+      getPlayerWrapper().style.display = "none";
+    }, 0);
+    getBbcArabicIframe().style.display = "block";
+    // .stop is not available in the free version, workaround: setting the file will stop playback
+    jwpInstance.setup({
+      ...jwtConfigSetup,
+      file: channels.find((c) => c.channelName === "Turis").src,
+    });
+  });
+
+  const showHideSidebar = document.querySelector(".show-hide-sidebar");
+  showHideSidebar.addEventListener("click", () => {
+    // User closed the sidebar : User opened the sidebar
+    channelsList.style.display =
+      channelsList.style.display !== "none" ? "none" : "flex";
+  });
+
+  const showHideInput = document.querySelector(".show-hide-input");
+  showHideInput.addEventListener("click", () => {
+    if (!inputWrapper.style.display) {
+      inputWrapper.style.display = "none";
+    }
+    // User closed the input : User opened the input
+    inputWrapper.style.display =
+      inputWrapper.style.display !== "none" ? "none" : "flex";
+  });
+
+  const createChannelItemAndAppendToList = ({ src, channelName }) => {
+    const channelItem = document.createElement("li");
+    channelItem.classList.add("channel-item");
+    channelItem.innerHTML = `<p class="channel-name" data-src="${src}">${channelName}</p>`;
+    channelsList.appendChild(channelItem);
+  };
+
+  function onLoad() {
+    channels.forEach((channelName) => {
+      createChannelItemAndAppendToList(channelName);
+    });
+  }
+
+  onLoad();
+
+  function play(_src, channelName) {
+    const playButton = document.querySelector("#play-button");
+    const setupJWPInstance = () => {
+      const src = _src || getSrcElement().value.trim();
+      if (src) {
+        jwpInstance.setup({ ...jwtConfigSetup, file: src });
+        // .play is not available in the free version of jwplayer
+        // jwpInstance.play();
+
+        document.title = channelName || "m3u8 player";
+      }
+    };
+    _src
+      ? setupJWPInstance()
+      : playButton.addEventListener("click", setupJWPInstance);
+    if (getPlayerWrapper().style.display === "none") {
+      getPlayerWrapper().style.display = "block";
+    }
+    if (_src !== "bbc-arabic") {
+      stopBBCPlayer();
+    }
+
+    handleHighlightingSelectedChannel(_src, channelsList);
+  }
+
+  play();
+
+  function playChannel() {
+    channelsList.addEventListener("click", (event) => {
+      if (event.target.closest(".turn-off")) {
+        const channelsList = document.querySelector("#channels-list");
+        jwpInstance.setup({
+          ...jwtConfigSetup,
+          file:
+            currentlyPlayingSrc ||
+            channels.find((c) => c.channelName === "Turis").src,
+        });
+        handleHighlightingSelectedChannel("", channelsList);
+        stopBBCPlayer();
+        setTimeout(() => {
+          getPlayerWrapper().style.display = "none";
+        }, 0);
+      } else if (event.target.getAttribute("data-src")) {
+        play(event.target.getAttribute("data-src"), event.target.textContent);
+      }
+    });
+  }
+
+  playChannel();
+}
+
+main();
---
+++ Ana-Dosya/10000-XXX/5-Player/script.js
@@ -0,0 +1,364 @@
+const channels = [
+  {
+    channelName: "XXX 201",
+    src: "https://vod.mycamtv.net/201.m3u8 ",
+  },
+  {
+    channelName: "XXX 202",
+    src: "https://vod.mycamtv.net/202.m3u8 ",
+  },
+    {
+    channelName: "XXX 203",
+    src: "https://vod.mycamtv.net/203.m3u8",
+  },
+    {
+    channelName: "XXX 204",
+    src: "https://vod.mycamtv.net/204.m3u8 ",
+  },
+    {
+    channelName: "XXX 205",
+    src: "https://vod.mycamtv.net/205.m3u8 ",
+  },
+    {
+    channelName: "XXX 206",
+    src: "https://vod.mycamtv.net/206.m3u8 ",
+  },
+    {
+    channelName: "XXX 207",
+    src: "https://vod.mycamtv.net/207.m3u8 ",
+  },
+    {
+    channelName: "XXX 208",
+    src: " https://vod.mycamtv.net/208.m3u8 ",
+  },
+     {
+    channelName: "XXX 209",
+    src: " https://vod.mycamtv.net/209.m3u8 ",
+  },
+     {
+    channelName: "XXX 210",
+    src: " https://vod.mycamtv.net/210.m3u8 ",
+  },
+     {
+    channelName: "XXX 211",
+    src: " https://vod.mycamtv.net/211.m3u8 ",
+  },
+     {
+    channelName: "XXX 212",
+    src: " https://vod.mycamtv.net/212.m3u8  ",
+  },
+     {
+    channelName: "XXX 213",
+    src: " https://vod.mycamtv.net/213.m3u8 ",
+  },
+     {
+    channelName: "XXX 214",
+    src: "https://vod.mycamtv.net/214.m3u8  ",
+  },
+     {
+    channelName: "XXX 215",
+    src: "https://vod.mycamtv.net/215.m3u8  ",
+  },
+     {
+    channelName: "XXX 216",
+    src: "https://vod.mycamtv.net/216.m3u8  ",
+  },
+     {
+    channelName: "XXX 217",
+    src: "https://vod.mycamtv.net/217.m3u8  ",
+  },
+     {
+    channelName: "XXX 218",
+    src: "https://vod.mycamtv.net/218.m3u8  ",
+  },
+     {
+    channelName: "XXX 219",
+    src: "https://vod.mycamtv.net/219.m3u8  ",
+  },
+     {
+    channelName: "XXX 220",
+    src: "https://vod.mycamtv.net/220.m3u8  ",
+  },
+     {
+    channelName: "XXX 221",
+    src: " https://vod.mycamtv.net/221.m3u8 ",
+  },
+     {
+    channelName: "XXX 222",
+    src: "https://vod.mycamtv.net/222.m3u8  ",
+  },
+     {
+    channelName: "XXX 223",
+    src: " https://vod.mycamtv.net/223.m3u8 ",
+  },
+     {
+    channelName: "XXX 224",
+    src: "https://vod.mycamtv.net/224.m3u8  ",
+  },
+     {
+    channelName: "XXX 225",
+    src: "https://vod.mycamtv.net/225.m3u8  ",
+  },
+     {
+    channelName: "XXX 226",
+    src: "https://vod.mycamtv.net/226.m3u8  ",
+  },
+     {
+    channelName: "XXX 227",
+    src: "https://vod.mycamtv.net/227.m3u8  ",
+  },
+     {
+    channelName: "XXX 228",
+    src: " https://vod.mycamtv.net/228.m3u8 ",
+  },
+     {
+    channelName: "XXX 229",
+    src: "https://vod.mycamtv.net/229.m3u8  ",
+  },
+     {
+    channelName: "XXX 230",
+    src: "https://vod.mycamtv.net/230.m3u8  ",
+  },
+     {
+    channelName: "XXX 231",
+    src: "https://vod.mycamtv.net/231.m3u8  ",
+  },
+     {
+    channelName: "XXX 232",
+    src: "https://vod.mycamtv.net/232.m3u8  ",
+  },
+     {
+    channelName: "XXX 233",
+    src: "https://vod.mycamtv.net/233.m3u8  ",
+  },
+  {
+    channelName: "XXX 234",
+    src: "https://vod.mycamtv.net/234.m3u8 ",
+  },
+  {
+    channelName: "Turis",
+    src: "https://vod.mycamtv.net/235.m3u8 ",
+  },
+  {
+    channelName: "XXX 236",
+    src: "https://vod.mycamtv.net/236.m3u8 ",
+  },
+  {
+    channelName: "XXX 237",
+    src: "https://vod.mycamtv.net/237.m3u8 ",
+  },
+  {
+    channelName: "XXX 238",
+    src: " https://vod.mycamtv.net/238.m3u8",
+  },
+  {
+    channelName: "XXX 239",
+    src: "https://vod.mycamtv.net/239.m3u8 ",
+  },
+  {
+    channelName: "XXX 240",
+    src: "https://vod.mycamtv.net/240.m3u8 ",
+  },
+  {
+    channelName: "XXX 241",
+    src: "https://vod.mycamtv.net/241.m3u8  ",
+  },
+  {
+    channelName: "XXX 242",
+    src: "https://vod.mycamtv.net/242.m3u8 ",
+  },
+  {
+    channelName: "XXX 243",
+    src: "https://vod.mycamtv.net/243.m3u8 ",
+  },
+  {
+    channelName: "XXX 244 ",
+    src: "https://vod.mycamtv.net/244.m3u8 ",
+  },
+  {
+    channelName: "XXX 245",
+    src: "https://vod.mycamtv.net/245.m3u8 ",
+  },
+  {
+    channelName: "XXX 246",
+    src: "https://vod.mycamtv.net/246.m3u8 ",
+  },
+  {
+    channelName: "XXX 247",
+    src: "https://vod.mycamtv.net/247.m3u8 ",
+  },
+  {
+    channelName: "XXX 248",
+    src: "https://vod.mycamtv.net/248.m3u8 ",
+  },
+
+  {
+    channelName: "XXX 249",
+    src: "https://vod.mycamtv.net/249.m3u8 ",
+  },
+
+  {
+    channelName: "XXX 250",
+    src: "https://vod.mycamtv.net/250.m3u8",
+  },
+];
+
+const jwtConfigSetup = {
+  file: channels.find((c) => c.channelName === "Turis").src,
+  width: "100%",
+  height: "100%",
+  skin: "glow",
+  autostart: true,
+  primary: "flash",
+  fallback: "false",
+  abouttext: "",
+  aboutlink: "",
+  displaytitle: "false",
+  ga: "{}",
+};
+
+const getPlayerWrapper = () =>
+  document.querySelector("#content #player-wrapper #jwplayer-wrapper");
+const getBbcArabicIframe = () => document.querySelector("#bbc-iframe");
+
+const getElementByDataSrc = (dataSrc) =>
+  document.querySelector(`[data-src="${dataSrc}"]`);
+
+let currentlyPlayingSrc = null;
+const handleHighlightingSelectedChannel = (_src, channelsList) => {
+  const currentElement = getElementByDataSrc(_src);
+  if (currentElement) {
+    [...channelsList.children].forEach((el) => {
+      const _el =
+        el.querySelector(".channel-name") || el.querySelector(".bbc-arabic");
+      if (_el?.getAttribute("id")?.includes("selected-channel-indicator")) {
+        _el.setAttribute(
+          "id",
+          _el.getAttribute("id").replace("selected-channel-indicator", "")
+        );
+      }
+    });
+    currentElement.setAttribute("id", "selected-channel-indicator");
+  }
+  if (_src) {
+    currentlyPlayingSrc = _src;
+  }
+};
+
+const stopBBCPlayer = () => {
+  setTimeout(() => {
+    getBbcArabicIframe().style.display = "none";
+    const iframeSrc = getBbcArabicIframe().src;
+    getBbcArabicIframe().src = iframeSrc;
+  }, 0);
+};
+
+function main() {
+  loadContent();
+}
+
+function loadContent() {
+  const jwpInstance = jwplayer("jwplayer-wrapper");
+  jwpInstance.setup(jwtConfigSetup);
+
+  const getSrcElement = () => document.querySelector("#player-src");
+  const channelsList = document.querySelector("#channels-list");
+  const inputWrapper = document.querySelector("#input-wrapper");
+
+  const bbcArabicButton = document.querySelector(".bbc-arabic");
+  bbcArabicButton.addEventListener("click", () => {
+    setTimeout(() => {
+      getPlayerWrapper().style.display = "none";
+    }, 0);
+    getBbcArabicIframe().style.display = "block";
+    // .stop is not available in the free version, workaround: setting the file will stop playback
+    jwpInstance.setup({
+      ...jwtConfigSetup,
+      file: channels.find((c) => c.channelName === "Turis").src,
+    });
+  });
+
+  const showHideSidebar = document.querySelector(".show-hide-sidebar");
+  showHideSidebar.addEventListener("click", () => {
+    // User closed the sidebar : User opened the sidebar
+    channelsList.style.display =
+      channelsList.style.display !== "none" ? "none" : "flex";
+  });
+
+  const showHideInput = document.querySelector(".show-hide-input");
+  showHideInput.addEventListener("click", () => {
+    if (!inputWrapper.style.display) {
+      inputWrapper.style.display = "none";
+    }
+    // User closed the input : User opened the input
+    inputWrapper.style.display =
+      inputWrapper.style.display !== "none" ? "none" : "flex";
+  });
+
+  const createChannelItemAndAppendToList = ({ src, channelName }) => {
+    const channelItem = document.createElement("li");
+    channelItem.classList.add("channel-item");
+    channelItem.innerHTML = `<p class="channel-name" data-src="${src}">${channelName}</p>`;
+    channelsList.appendChild(channelItem);
+  };
+
+  function onLoad() {
+    channels.forEach((channelName) => {
+      createChannelItemAndAppendToList(channelName);
+    });
+  }
+
+  onLoad();
+
+  function play(_src, channelName) {
+    const playButton = document.querySelector("#play-button");
+    const setupJWPInstance = () => {
+      const src = _src || getSrcElement().value.trim();
+      if (src) {
+        jwpInstance.setup({ ...jwtConfigSetup, file: src });
+        // .play is not available in the free version of jwplayer
+        // jwpInstance.play();
+
+        document.title = channelName || "m3u8 player";
+      }
+    };
+    _src
+      ? setupJWPInstance()
+      : playButton.addEventListener("click", setupJWPInstance);
+    if (getPlayerWrapper().style.display === "none") {
+      getPlayerWrapper().style.display = "block";
+    }
+    if (_src !== "bbc-arabic") {
+      stopBBCPlayer();
+    }
+
+    handleHighlightingSelectedChannel(_src, channelsList);
+  }
+
+  play();
+
+  function playChannel() {
+    channelsList.addEventListener("click", (event) => {
+      if (event.target.closest(".turn-off")) {
+        const channelsList = document.querySelector("#channels-list");
+        jwpInstance.setup({
+          ...jwtConfigSetup,
+          file:
+            currentlyPlayingSrc ||
+            channels.find((c) => c.channelName === "Turis").src,
+        });
+        handleHighlightingSelectedChannel("", channelsList);
+        stopBBCPlayer();
+        setTimeout(() => {
+          getPlayerWrapper().style.display = "none";
+        }, 0);
+      } else if (event.target.getAttribute("data-src")) {
+        play(event.target.getAttribute("data-src"), event.target.textContent);
+      }
+    });
+  }
+
+  playChannel();
+}
+
+main();
