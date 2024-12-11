7d441eb9c5acae5e08c74c001dd1c73893da94a4
Turis-Tv <turistv3@gmail.com>
2024-12-11 11:10:09 +0100

    Add files via upload

---
+++ Ana-Dosya/20-Test-Sayfasi/4-Player-Menu/script.js
@@ -0,0 +1,159 @@
+var playerInstance = jwplayer("jwplayerDiv");
+
+// Function to switch to another stream with M3U8 URL and no DRM
+  function switchToOtherStream1() {
+    playerInstance.setup({
+      file: "http://cdn.adultiptv.net/anal.m3u8",
+      type: "hls",  // Set the type to HLS for M3U8 URLs
+    });
+  }
+
+  // Function to switch to another stream with M3U8 URL and no DRM
+  function switchToOtherStream2() {
+    playerInstance.setup({
+      file: "http://cdn.adultiptv.net/asian.m3u8",
+      type: "hls",  // Set the type to HLS for M3U8 URLs
+    });
+  }
+// Function to switch to another stream with M3U8 URL and no DRM
+  function switchToOtherStream3() {
+    playerInstance.setup({
+      file: "http://cdn.adultiptv.net/bigass.m3u8",
+      type: "hls",  // Set the type to HLS for M3U8 URLs
+    });
+  }
+// Function to switch to another stream with M3U8 URL and no DRM
+  function switchToOtherStream4() {
+    playerInstance.setup({
+      file: "http://cdn.adultiptv.net/bigdick.m3u8",
+      type: "hls",  // Set the type to HLS for M3U8 URLs
+    });
+  }
+// Function to switch to another stream with M3U8 URL and no DRM
+  function switchToOtherStream5() {
+    playerInstance.setup({
+      file: "https://production-binge-content-partner.videoready.tv/RebelHudugaru_Movie/AppleHLS1/RebelHudugaru_Movie_hls3000.m3u8",
+      type: "hls",  // Set the type to HLS for M3U8 URLs
+    });
+  }
+// Function to switch to another stream with M3U8 URL and no DRM
+  function switchToOtherStream6() {
+    playerInstance.setup({
+      file: "https://production-binge-content-partner.videoready.tv/BB5_Movie/AppleHLS1/BB5_Movie_hls3000.m3u8",
+      type: "hls",  // Set the type to HLS for M3U8 URLs
+    });
+  }
+
+// Function to switch to another stream with M3U8 URL and no DRM
+  function switchToOtherStream7() {
+    playerInstance.setup({
+      file: "https://production-binge-content-partner.videoready.tv/StyleRaja_Movie/AppleHLS1/StyleRaja_Movie_hls3000.m3u8",
+      type: "hls",  // Set the type to HLS for M3U8 URLs
+    });
+  }
+// Function to switch to another stream with M3U8 URL and no DRM
+  function switchToOtherStream8() {
+    playerInstance.setup({
+      file: "https://mavenarts.in/jio/app/live/429/Colors_Tamil_HD/a0148df055d2e1e51f22cfd5e9aec4a73a1a2e64d8581a64fec4ad52a4b5ba26d353e722a23360b8d069c766d70b2bb678f6c713bf053e5f81bf80648ef7344ad5da400ac2a16719b3b410afbb286c6f58d901dda9cfcb68f87cbc53bb0ffb925c4d98eeb9c27b269a8b879d563c483557f014ae07428d33a698194284e14c04_playlist.m3u8",
+      type: "hls",  // Set the type to HLS for M3U8 URLs
+    });
+  }
+
+
+
+
+// Function to switch to another stream with M3U8 URL and no DRM
+  function switchToOtherStream9() {
+    playerInstance.setup({
+      file: "https://mavenarts.in/jio/app/live/557/Chutti_TV/a0148df055d2e1e51f22cfd5e9aec4a73a1a2e64d8581a64fec4ad52a4b5ba26df1839a817bea36f5af626dd638690baadb366cd9f8f64ad7554484e18504c9059cba930e1c39f6a0af1650c743a07fbddbbfad5c2fac65f8c6c84a2eec9a331d374fc2cd7d78ec31edd8907d5592c58298c5baa5da6b4ce74b7136ab07c46ff_playlist.m3u8",
+      type: "hls",  // Set the type to HLS for M3U8 URLs
+    });
+  }
+
+// Function to switch to another stream with M3U8 URL and no DRM
+  function switchToOtherStream10() {
+    playerInstance.setup({
+      file: "https://mavenarts.in/jio/app/live/615/News_18_Tamilnadu/a0148df055d2e1e51f22cfd5e9aec4a73a1a2e64d8581a64fec4ad52a4b5ba268a21f643a39f14c4b6bba30459594c737eb3807b72a27adeb7d03347e0fc99461eee4bb8b05e8cc3ec003a581387ef029ee8673329a9d6267dcef8b638851f08b8205236d501fc6d5b2ce7d0c94c89db55060c24285b4d733d2e03fcfa61e038e735a7baf43d1cf1226667d2c763b441_playlist.m3u8",
+      type: "hls",  // Set the type to HLS for M3U8 URLs
+    });
+  }
+
+// Function to switch to another stream with M3U8 URL and no DRM
+  function switchToOtherStream11() {
+    playerInstance.setup({
+      file: "https://mavenarts.in/jio/app/live/682/Sun_Life/a0148df055d2e1e51f22cfd5e9aec4a73a1a2e64d8581a64fec4ad52a4b5ba26680044c97fbd54fb5d0740445f95bcb85fe4c7677fdfeada7d35578fae8c6f6c8e66d9723082d37c808b1b197f90c6060c8f4f0c2ea9efae3651c7d7a6ac74b93be7d60ce69c04ed3e78ca4a9cb0e7aa391580b61907ea1134e72360a0ecfb1d_playlist.m3u8",
+      type: "hls",  // Set the type to HLS for M3U8 URLs
+    });
+  }
+// Function to switch to another stream with M3U8 URL and no DRM
+  function switchToOtherStream12() {
+    playerInstance.setup({
+      file: "https://mavenarts.in/jio/app/live/683/Raj_Digital_Plus/a0148df055d2e1e51f22cfd5e9aec4a73a1a2e64d8581a64fec4ad52a4b5ba26af27a3ce2d4a1be37455e38100498bee93bfdd0f9fcd6f41a5cd7ee2ae419d49b460998b42b28b6bb0e7c89ae2ac7d07c7739d573c22b3a9daa09e8deb3382424c89b2e8db429ea5bb65eedff52bab63be0724b578d714146083dc295e2a61971e247b562a8242e0612739a14c8dea50_playlist.m3u8",
+      type: "hls",  // Set the type to HLS for M3U8 URLs
+    });
+  }
+// Function to switch to another stream with M3U8 URL and no DRM
+  function switchToOtherStream13() {
+    playerInstance.setup({
+      file: "https://mavenarts.in/jio/app/live/705/Polimer_TV/a0148df055d2e1e51f22cfd5e9aec4a73a1a2e64d8581a64fec4ad52a4b5ba26e79293944fe707371850d4783a8c865a25750e625d035186d5215605e4510a16d8e796e581204b8e047d4287f372fa969a31732a24f0fcbc1d4d02d7b87cfbe0ad23f46f3e58a53d5f5eb00b6319dd5127f26f3abfd4bcad20e554118a01f0ea_playlist.m3u8",
+      type: "hls",  // Set the type to HLS for M3U8 URLs
+    });
+  }
+// Function to switch to another stream with M3U8 URL and no DRM
+  function switchToOtherStream14() {
+    playerInstance.setup({
+      file: "https://mavenarts.in/jio/app/live/895/Sun_Music_HD/a0148df055d2e1e51f22cfd5e9aec4a73a1a2e64d8581a64fec4ad52a4b5ba26289ec3b8fe433dd574373da3917ff13af094e7d1db816799a0fc7f913da8aef91e84e71d60f2c2a4ca560ae636293509c01dafd51b739bf51624a28ae45a50ac46abe1873aff849bc91bf0d8912eaa4c6a7ee52f29406c9723d16dce55cb2bad_playlist.m3u8",
+      type: "hls",  // Set the type to HLS for M3U8 URLs
+    });
+  }
+
+// Function to switch to another stream with M3U8 URL and no DRM
+  function switchToOtherStream15() {
+    playerInstance.setup({
+      file: "https://mavenarts.in/jio/app/live/1209/Kalaignar_TV/a0148df055d2e1e51f22cfd5e9aec4a73a1a2e64d8581a64fec4ad52a4b5ba26b3b988a64e15a681bfc09a983137418475331a93ef4ab39c94d15d52f6426cab7f34946fa4a6afe4c40c3e9fca51132ba5d3b5ea0860f467e69dd8a474c0003434b31567aa0fb4eb42b5ecac666fc0ebc78ad3b808b8d5d715205dc6468ab136_playlist.m3u8",
+      type: "hls",  // Set the type to HLS for M3U8 URLs
+    });
+  }
+// Function to switch to another stream with M3U8 URL and no DRM
+  function switchToOtherStream16() {
+    playerInstance.setup({
+      file: "https://mavenarts.in/jio/app/live/2256/Zee_Thirai/a0148df055d2e1e51f22cfd5e9aec4a73a1a2e64d8581a64fec4ad52a4b5ba269fa34f82e1d231a22bdb5625480a17991d88e70ea7e2231e4b3d4259f43328d14dd8b623523cc7baff0ae17bcba5187c3238b8aecdfc515501e13bf20b8403c8aafe7cd98c77e56bd75407b72227a7da9881d462af5f9017322b6b4db65a4c26_playlist.m3u8",
+      type: "hls",  // Set the type to HLS for M3U8 URLs
+    });
+  }
+// Function to switch to another stream with M3U8 URL and no DRM
+  function switchToOtherStream17() {
+    playerInstance.setup({
+      file: "https://mavenarts.in/jio/app/live/971/Vaanavil_TV/a0148df055d2e1e51f22cfd5e9aec4a73a1a2e64d8581a64fec4ad52a4b5ba26ff15b54e01827d3bd571ccc8d394bf5b47d5ddf193b2014a5c1d9d78b74ee0d1ff1756b3616976230f4c091772bffa1b4684a5614bd675f35497a2308139e2a9a55f85456857fe135fc5e140192f69772c6975ebb73190aedc0044d3ac226f7e_playlist.m3u8",
+      type: "hls",  // Set the type to HLS for M3U8 URLs
+    });
+  }
+
+// Function to switch to another stream with M3U8 URL and no DRM
+  function switchToOtherStream18() {
+    playerInstance.setup({
+      file: "https://mavenarts.in/jio/app/live/847/MK_TV/a0148df055d2e1e51f22cfd5e9aec4a73a1a2e64d8581a64fec4ad52a4b5ba267854873647b5778e1b42f649a6329252ac6ed9b96b90a8c1352b0c09d3fcb37e30a0147db1ed43a5fdb4a812a56c6884f7496d3c43c17e0b211b471909721139383c29180141f07b7561e6fe2d8124ba_playlist.m3u8",
+      type: "hls",  // Set the type to HLS for M3U8 URLs
+    });
+  }
+
+// Function to switch to another stream with M3U8 URL and no DRM
+  function switchToOtherStream19() {
+    playerInstance.setup({
+      file: "https://mavenarts.in/jio/app/live/857/Vendhar_TV/a0148df055d2e1e51f22cfd5e9aec4a73a1a2e64d8581a64fec4ad52a4b5ba267904e305ad6c296228fb55b5824a652755eaa21e03e7d12b829a342d48df830e1693526176ca3001ab645d07b3901f175c8a6b70cb00e93b0e010573734a790ea4499d71e83efdcb68889a4a77de7110d443363d364fcdcff3b4ece1f097df76_playlist.m3u8",
+      type: "hls",  // Set the type to HLS for M3U8 URLs
+    });
+  }
+
+   function switchToDRM() {
+     playerInstance.setup({
+         file: "#",
+           type: "hls",  // Set the type to HLS for M3U8 URLs
+         });
+         }
+  function switchToDRM() {
+    playerInstance.setup({
+        file: "https://isp.sananda.online/Star/video.m3u8",
+          type: "hls",  // Set the type to HLS for M3U8 URLs
+        });
+        }
